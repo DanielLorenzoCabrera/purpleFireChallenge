9d074ff0703e4d7fc8e5e8f3a2043446608d31e4
Daniel Lorenzo <daniel@xrealityfactory.com>
2024-04-07 12:30:11 +0100

    refactor: Added new fonts to the project. Implemented first navbar desktop style.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,47 @@
 import Navbar from "@/components/Navbar";
 import Dropdown from "@/components/Dropdown";
 
-const Dropdownoptions = [
+const langOtions = [
   {
-    label: "option 0",
-    value: "option 0",
+    label: "ENG",
+    value: "ENG",
   },
   {
-    label: "option 1",
-    value: "option 1",
+    label: "SPA",
+    value: "SPA",
+  },
+  {
+    label: "FRE",
+    value: "FRE",
+  },
+];
+
+const currencyOptions = [
+  {
+    label: "USD",
+    value: "USD",
+  },
+  {
+    label: "EUR",
+    value: "EUR",
+  },
+  {
+    label: "GPB",
+    value: "GPB",
   },
 ];
 
 export default function DetailPage() {
   return (
     <main>
-      <Navbar>
-        <Dropdown options={Dropdownoptions} />
+      <Navbar customClass="lang-currency">
+        <>
+          <span>Welcome to Needus & Get the best product</span>
+          <section>
+            <Dropdown options={langOtions} customClass="lang" />
+            <Dropdown options={currencyOptions} customClass="currency" />
+          </section>
+        </>
       </Navbar>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 interface NavbarProps {
   children?: JSX.Element;
+  customClass?: string;
 }
 
-const Navbar = ({ children }: NavbarProps): JSX.Element => {
-  return <nav>{children}</nav>;
+const Navbar = ({ children, customClass }: NavbarProps): JSX.Element => {
+  return <nav className={customClass}>{children}</nav>;
 };
 
 export default Navbar;
